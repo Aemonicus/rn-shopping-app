00dd75943e73efaa29e8c0e6f45d02f3faae293e
Florent VICENTE <vicentegroupe@gmail.com>
2021-04-29 22:14:23 +0200

    setting up a navigator

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,10 +1,19 @@
 import React from 'react'
-import { FlatList } from "react-native"
+import { FlatList, Text } from "react-native"
+// Hook permettant d'aller chercher dans redux
+import { useSelector } from "react-redux"
 
 const ProductsOverviewScreen = () => {
+  const products = useSelector(state => state.products.availableProducts)
   return (
-    <FlatList />
+    <FlatList
+      data={products}
+      renderItem={itemData => <Text>{itemData.item.title}</Text>} />
   )
+}
+
+ProductsOverviewScreen.navigationOptions = {
+  headerTitle: "All Products"
 }
 
 export default ProductsOverviewScreen
