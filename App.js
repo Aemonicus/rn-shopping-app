bd9f361ef4be5939f195f87af2258f548b8cc1f6
Florent VICENTE <vicentegroupe@gmail.com>
2021-04-29 21:32:56 +0200

    redux setup with products overview screen + small export fix

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ export default function App() {
   return (
     <Provider store={store}>
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>YOLOOOOOOOOOO!</Text>
       </View>
     </Provider>
   );
