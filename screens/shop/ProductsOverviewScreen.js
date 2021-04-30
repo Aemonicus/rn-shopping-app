8474dc6966edd935b095e74502a1c7c7c12bcb2c
Florent VICENTE <vicentegroupe@gmail.com>
2021-04-30 11:18:16 +0200

    styling product item

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import { FlatList, Text } from "react-native"
-// Hook permettant d'aller chercher dans redux
+// Hook permettant d'aller chercher dans le state
 import { useSelector } from "react-redux"
+
+import ProductItem from "../../components/shop/ProductItem"
 
 const ProductsOverviewScreen = () => {
   const products = useSelector(state => state.products.availableProducts)
   return (
     <FlatList
       data={products}
-      renderItem={itemData => <Text>{itemData.item.title}</Text>} />
+      renderItem={itemData =>
+        <ProductItem
+          image={itemData.item.imageUrl}
+          title={itemData.item.title}
+          price={itemData.item.price}
+          onViewDetail={() => { }}
+          onAddToCart={() => { }}
+        />} />
   )
 }
 
