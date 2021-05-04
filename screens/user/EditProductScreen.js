4bda696c0431816f63a04be6a3b2f580f32e9796
Florent VICENTE <vicentegroupe@gmail.com>
2021-05-04 11:12:49 +0200

    adding basic editing and navigation logic

---
+++ screens/user/EditProductScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text, StyleSheet } from "react-native"
+
+const EditProductScreen = () => {
+  return (
+    <Text>Edit product screen</Text>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default EditProductScreen
--- screens/user/UserProductsScreen.js
+++ screens/user/UserProductsScreen.js
@@ -11,9 +11,14 @@ import Colors from "../../constants/Colors"
 import * as productsActions from "../../store/actions/products"
 
 
-const UserProductsScreen = () => {
+const UserProductsScreen = ({ navigation }) => {
   const userProducts = useSelector(state => state.products.userProducts)
   const dispatch = useDispatch()
+
+  const editProductHandler = id => {
+    navigation.navigate("EditProduct", { productId: id })
+  }
+
   return (
     <FlatList
       data={userProducts}
@@ -21,9 +26,9 @@ const UserProductsScreen = () => {
         image={itemData.item.imageUrl}
         title={itemData.item.title}
         price={itemData.item.price}
-        onSelect={() => { }}
+        onSelect={() => { editProductHandler(itemData.item.id) }}
       >
-        <Button color={Colors.primary} title="Edit" onPress={() => { }} />
+        <Button color={Colors.primary} title="Edit" onPress={() => { editProductHandler(itemData.item.id) }} />
         <Button color={Colors.primary} title="Delete" onPress={() => { dispatch(productsActions.deleteProduct(itemData.item.id)) }} />
       </ProductItem>}
     />
@@ -33,9 +38,22 @@ const UserProductsScreen = () => {
 UserProductsScreen.navigationOptions = navData => {
   return {
     headerTitle: "Your Products",
-    headerLeft: <HeaderButtons HeaderButtonComponent={HeaderButton}>
-      <Item title="Menu" iconName={Platform.OS === "android" ? "md-menu" : "ios-menu"} onPress={() => { navData.navigation.toggleDrawer() }} />
-    </HeaderButtons>,
+    headerLeft: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Menu"
+          iconName={Platform.OS === "android" ? "md-menu" : "ios-menu"}
+          onPress={() => { navData.navigation.toggleDrawer() }} />
+      </HeaderButtons>
+    ),
+    headerRight: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Add"
+          iconName={Platform.OS === "android" ? "md-create" : "ios-create"}
+          onPress={() => { navData.navigation.navigate("EditProduct") }} />
+      </HeaderButtons>
+    )
   }
 
 }
